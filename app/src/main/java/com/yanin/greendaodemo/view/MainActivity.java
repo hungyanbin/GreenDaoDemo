c3e5898f20461735d52835ba3ce4f7773598e9d6
YANBIN <yanbin.hung@eyesmedia.tw>
2016-09-22 15:40:38 +0800

    add scroll behavior

--- app/src/main/java/com/yanin/greendaodemo/view/MainActivity.java
+++ app/src/main/java/com/yanin/greendaodemo/view/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -39,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         studentDao = dbService.getStudentDao();
         setupRecycleView();
         setupFAB();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.title_student));
+        setSupportActionBar(toolbar);
     }
 
     private void setupFAB() {
--- app/src/main/java/com/yanin/greendaodemo/view/TeacherActivity.java
+++ app/src/main/java/com/yanin/greendaodemo/view/TeacherActivity.java
@@ -2,6 +2,7 @@ package com.yanin.greendaodemo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.yanin.greendaodemo.R;
 
@@ -11,5 +12,9 @@ public class TeacherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
-    }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.title_teacher));
+        setSupportActionBar(toolbar);
+}
 }
